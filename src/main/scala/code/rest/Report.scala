ce69eae90e68f0dfd3edd6dae6579630c304664f
Richard Dallaway <richard@dallaway.com>
2013-04-16 21:18:07 +0100

    Writing report directly to servlet output.

--- src/main/scala/code/rest/Report.scala
+++ src/main/scala/code/rest/Report.scala
@@ -1,8 +1,12 @@
 package code.rest
 
 import net.liftweb.http.rest.RestHelper
-import net.liftweb.http.{OutputStreamResponse, Req, LiftRules}
+import net.liftweb.http._
 import java.io.{BufferedInputStream, FileInputStream}
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager
+import java.net.URL
+import org.pentaho.reporting.engine.classic.core.MasterReport
+import org.pentaho.reporting.engine.classic.core.modules.output.pageable.pdf.PdfReportUtil
 
 object Report extends RestHelper {
 
@@ -11,6 +15,16 @@ object Report extends RestHelper {
   }
 
   serve {
+
+    case Req("example" :: Nil, _, _) =>
+      val manager = new ResourceManager
+      manager.registerDefaults
+      val reportPath = "file:reports/report.prpt"
+      val res = manager.createDirectly(new URL(reportPath), classOf[MasterReport])
+      val report = res.getResource.asInstanceOf[MasterReport]
+      OutputStreamResponse(out => PdfReportUtil.createPDF(report, out))
+
+
     case Req("report" :: file :: Nil, _, _) =>
       val fileName = "output/" + file + ".pdf"
       val bis = new BufferedInputStream(new FileInputStream(fileName)) //TODO: check if file exists
